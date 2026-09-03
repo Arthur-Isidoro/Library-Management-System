e552deb0bd705db2bea723ae5295054de8726baf
ArtinhasDev <arthurif2011@hotmail.com>
2026-09-02 21:41:12 -0300

    Update TesteEnv.java

--- src/main/java/com/seuprojeto/library/TesteEnv.java
+++ src/main/java/com/seuprojeto/library/TesteEnv.java
@@ -2,6 +2,7 @@ package com.seuprojeto.library;
 
 public class TesteEnv {
     public static void main(String[] args) {
+        System.out.println("Dados do banco de dados:");
         System.out.println("Usuario: " + System.getenv("DB_USUARIO"));
         System.out.println("Senha: " + System.getenv("DB_SENHA"));
     }
